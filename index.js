d5faeee5657cab186f9eb6f2ff510476c36efbe5
Krishna Gurung <gkrishnabahadur618@gmail.com>
2024-09-24 22:48:25 +0545

    adding toggle button

--- index.js
+++ index.js
@@ -53,3 +53,22 @@ form.addEventListener('submit', (e) => {
         inputBox.value = ''; // Clear the input box after submitting
     }
 });
+
+
+const toggleButton = document.getElementById('dark-mode-toggle'); // The dark mode toggle button
+const body = document.body;
+let cur="light";
+let icon=document.querySelector('#icon');
+
+
+toggleButton.addEventListener("click",()=>{
+    if(cur=="light"){
+        body.classList.add('dark-mode');
+        cur="dark";
+        icon.src="sun.png";
+    }else{
+        body.classList.remove('dark-mode');
+        cur="light";
+        icon.src="moon.png";
+    }
+});
